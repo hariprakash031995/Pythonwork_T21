3cbfd271d16574fac656afcaa269470e9a4ba21f
hariprakash031995 <hariprakash031995@gmail.com>
2023-11-08 15:59:22 +0530

    Updte

--- Practice_python/list1.py
+++ Practice_python/list1.py
@@ -3,7 +3,16 @@
 name = ["hari", "John", "arun", "le", "don", "a"]
 
 for n in name:
-    a = 2 <= len(n) < 3
-    if a is True:
-        print(n)
-    
+    if len(n)==2:
+        print(n, "This is two digit length")
+
+    # else:
+    #     print(n, "is not a 2 digit string")
+
+
+print("***********************************")
+
+name.pop(0)
+print(name)
+
+print("***********************************")
