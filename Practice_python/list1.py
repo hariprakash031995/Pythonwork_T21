fa841e2ebbd1ff250dd12464fe53e18b97264c7b
hariprakash031995 <hariprakash031995@gmail.com>
2023-12-15 15:26:08 +0530

    Update

--- Practice_python/list1.py
+++ Practice_python/list1.py
@@ -51,3 +51,79 @@ if lis:
 else:
     print("List is empty")
 
+print("***********************************")
+
+# Remove duplicates
+
+a = [1, 1, 2, 3, 4, 5, 6, 7, 1, 8, 9, 10]
+
+z = set(a)
+print(list(z))
+
+# Smallest number from list
+print("*******************************")
+#1
+a = [1, -1, 2, 3, 4, 5, 6, 7, 1, 8, 9, 10]
+print(min(a), max(a), "\n\n")
+#2
+smallest_number = None
+for number in a:
+    if smallest_number is None or number < smallest_number:
+        smallest_number = number
+
+print(smallest_number)
+#3
+larg_num = None
+for num in a:
+    if larg_num is None or num > larg_num:
+        larg_num = num
+
+print(larg_num)
+
+
+# Get a string made of the first 2 and the last 2 chars from a given a string
+
+print("*******************************")
+
+a = "Hari"
+#print(len(a))
+if len(a) < 2:
+    print("Empty string")
+else:
+    print(a[0:2], a[-2:])
+
+print("*******************************")
+
+# Sum of elements
+# Mulitply of elements
+
+a = [1, 1, 1, 1, -5]
+z = 0
+for n in a:
+    if n:
+        z = z + n
+print(z)
+
+for n in a:
+    if n:
+        z = z * n
+print(z)
+
+print("*******************************")
+#  Clone or copy
+a= ["hari"]
+print(a)
+z = a.copy()
+print(z)
+
+print("***********************************")
+
+def by_length(fruit):
+  return len(fruit)
+
+fruits = ['apple', 'orange', 'banana', "hh", "njh"]
+fruits.sort(key = by_length)
+print(fruits)
+
+
+
